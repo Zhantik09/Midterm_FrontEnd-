5be900df3ec94373e10f105c4099320be79e86ea
Zhantik09 <zk2085@nyu.edu>
2023-10-12 08:53:46 -0400

    Updt1

--- Midterm/garden.js
+++ Midterm/garden.js
@@ -1,15 +1,15 @@
-document.getElementById("plant1").addEventListener("click", function() {
-    document.getElementById("plant-details").innerText = "Plant 1: Description of Plant 1.";
+document.getElementById("plant6").addEventListener("click", function() {
+    document.getElementById("plant-details").innerText = "This is Black-eyed Susan. This sunny flower will make you smile, plus it doesn't need babied because it's drought tolerant and blooms for weeks and weeks.";
 });
 
-document.getElementById("plant2").addEventListener("click", function() {
-    document.getElementById("plant-details").innerText = "Plant 2: Description of Plant 2.";
+document.getElementById("plant7").addEventListener("click", function() {
+    document.getElementById("plant-details").innerText = "This is Chrysanthemums. Plant these perennials in the spring so their roots can get established and survive winter; if you wait to fall to plant, they'll give a good show but likely won't return next year because they're putting their energy into blooming, not putting down roots.";
 });
 
-document.getElementById("plant3").addEventListener("click", function() {
-    document.getElementById("plant-details").innerText = "Plant 3: Description of Plant 3.";
+document.getElementById("plant8").addEventListener("click", function() {
+    document.getElementById("plant-details").innerText = "This is Crocus. These tiny flowers pop up in early spring, often when snow still is on the ground. Plant in the fall for spring blooms that return year after year.";
 });
 
-document.getElementById("plant4").addEventListener("click", function() {
-    document.getElementById("plant-details").innerText = "Plant 4: Description of Plant 4.";
+document.getElementById("plant9").addEventListener("click", function() {
+    document.getElementById("plant-details").innerText = "This is Daffodil. Their bobbing heads are one of the earliest signs of spring--plus, rodents won't bother these bulbs (unlike crocuses). Plant in the fall for spring flowers, which should return for many years.";
 });
